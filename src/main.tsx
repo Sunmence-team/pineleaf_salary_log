5333eb8941d5163e32a434e692f8210956ff0d8d
Mayorkun27 <156526127+Mayorkun27@users.noreply.github.com>
2025-11-05 18:02:29 +0100

    changed router type to browser not hash

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { HashRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { UserProvider } from './context/UserContext'
 
 createRoot(document.getElementById('root')!).render(
